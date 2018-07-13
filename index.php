bcc665fca003b6a663d068f024124d43df80c667
Rahmani Saif El Moulouk <cool2309@gmail.com>
2018-07-13 17:34:26 +0100

    Adding footer and change some styling

--- index.php
+++ index.php
@@ -8,9 +8,9 @@
     <link rel="stylesheet" type="text/css" media="screen" href="/dist/app.css"/>
 </head>
 <body>
-<div id="app">
-    <nav class="navbar navbar-expand-lg navbar-light bg-light">
-        <a class="navbar-brand" href="/">Weather</a>
+<div id="app" class="d-flex flex-column justify-content-center align-content-between">
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark text-white">
+        <a class="navbar-brand text-white" href="/">Weather</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse"
                 data-target="#navbarSupportedContent"
                 aria-controls="navbarSupportedContent" aria-expanded="false"
@@ -21,12 +21,18 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto">
                 <li class="nav-item active">
-                    <router-link to="/" class="nav-link">Home</router-link>
+                    <router-link to="/" class="nav-link text-white">Home</router-link>
                 </li>
             </ul>
         </div>
     </nav>
     <router-view :key="$route.fullPath"></router-view>
+
+    <footer class="footer bg-dark text-warning">
+        <div class="container">
+            <span class="text-muted">Made with <span style="color: red;">&#9829;</span> (love) by Rahmani Saif El Moulouk</span>
+        </div>
+    </footer>
 </div>
 <script src="/dist/app.js"></script>
 </body>
